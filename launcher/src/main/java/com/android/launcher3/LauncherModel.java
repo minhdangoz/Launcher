0f5df02106fd4de633b0d115e7c63761b6145123
Yanni Wen <yanniwen8@gmail.com>
2016-05-04 00:06:46 +0800

    support for oppo R9

--- launcher/src/main/java/com/android/launcher3/LauncherModel.java
+++ launcher/src/main/java/com/android/launcher3/LauncherModel.java
@@ -4917,7 +4917,10 @@ public class LauncherModel extends BroadcastReceiver
             "cn.lt.game",
             "net.dx.cloudgame",
             "com.dx.agent2",
-            "com.shyz.desktop"
+            "com.shyz.desktop",
+            "com.xiaomi.market",
+            "com.xiaomi.gamecenter",
+            "com.sohu.inputmethod.sogou"
     };
     
     private boolean isFilterPackage(String pkg) {
