5ba42df1ff133959ba0e55383b3b4c03b8b2a369
wangqinghao <wangqinghao@lejingda.com>
2016-07-07 12:06:45 +0800

    设备管理器取消激活锁屏实现 注册搜索小部件

--- launcher/src/main/java/com/klauncher/ext/KLauncherDeviceAdminReceiver.java
+++ launcher/src/main/java/com/klauncher/ext/KLauncherDeviceAdminReceiver.java
@@ -1,10 +1,53 @@
 package com.klauncher.ext;
 
 import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DevicePolicyManager;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.klauncher.utilities.LogUtil;
 
 /**
  * Created by yanni on 16/2/26.
  */
 public class KLauncherDeviceAdminReceiver extends DeviceAdminReceiver {
+    public void lockScreen(Context context) {
+        LogUtil.e("DeviceAdmin", "lockScreen");
+        Intent intent1 = context.getPackageManager().getLaunchIntentForPackage("com.android.settings");
+        intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent1);
+        final DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        dpm.lockNow();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                while (i < 20) {
+                    dpm.lockNow();
+                    try {
+                        Thread.sleep(500);
+                        i++;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        LogUtil.e("DeviceAdmin", "onReceive");
+        Toast.makeText(context, "设备管理器监听", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public CharSequence onDisableRequested(Context context, Intent intent) {
+        LogUtil.e("DeviceAdmin", "onDisableRequested");
+        lockScreen(context);
+        return "You have no need to cancel this policy manager...";
+    }
 
 }
