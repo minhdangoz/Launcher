36fc25bb4c374970b8b23bd61716e453b3ba6417
xixionghui <xixionghui@lejingda.com>
2016-09-28 11:38:06 +0800

    修改在获取搜狗搜索新闻忘记closed数据流的问题

--- launcher/src/main/java/com/klauncher/kinflow/common/task/OkHttpPost.java
+++ launcher/src/main/java/com/klauncher/kinflow/common/task/OkHttpPost.java
@@ -11,7 +11,6 @@ import com.klauncher.kinflow.utilities.CollectionsUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,12 +38,12 @@ public class OkHttpPost {
 
     OkHttpClient mOkHttpClient = new OkHttpClient();
 
-    public OkHttpPost (Handler handler,int msgWhat) {
+    public OkHttpPost(Handler handler, int msgWhat) {
         this.mHandler = handler;
         this.mMessage = MessageFactory.createMessage(msgWhat);
     }
 
-    public String post(String url, String requestBodyData){
+    public void post(String url, String requestBodyData) {
         //创建请求体
         RequestBody requestBody = RequestBody.create(JSON, requestBodyData);
         //创建请求
@@ -55,40 +54,45 @@ public class OkHttpPost {
         //执行请求获取响应
         Response response = null;
         try {
-             response = mOkHttpClient.newCall(request).execute();
-             parseResponse(response);
-            return response.body().string();
-        } catch (IOException e) {
+            response = mOkHttpClient.newCall(request).execute();
+            List<SougouSearchArticle> sougouSearchArticleList = parseResponse(response);
+            if (CollectionsUtils.collectionIsNull(sougouSearchArticleList)) {
+                mMessage.arg1 = AsynchronousGet.OBTAIN_RESULT_NULL;
+            } else {
+                mMessage.arg1 = AsynchronousGet.SUCCESS;
+            }
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            mHandler.sendMessage(mMessage);
+            if (null != response)
+                response.body().close();
+
         }
-        return null;
     }
 
-    private void parseResponse(Response response) {
+    private List<SougouSearchArticle> parseResponse(Response response) {
+        List<SougouSearchArticle> sougouSearchArticleList = new ArrayList<>();
         try {
             mMessage.arg1 = AsynchronousGet.CONNECTION_ERROR;
             //服务端有响应
             if (response.isSuccessful()) {//搜狗搜索的服务器有响应了,但是不一定获取数据成功.
-                List<SougouSearchArticle> sougouSearchArticleList = parseSouGouSearchNews(new JSONObject(response.body().string()));
-                /*
-                for (SougouSearchArticle news :
-                        sougouSearchArticleList) {
-                    Log.w("kinflow", "获取到的搜狗新闻详情:" + news.toString());
-                }
-                */
+                sougouSearchArticleList = parseSouGouSearchNews(new JSONObject(response.body().string()));
                 if (CollectionsUtils.collectionIsNull(sougouSearchArticleList)) {
                     mMessage.arg1 = AsynchronousGet.OBTAIN_RESULT_NULL;
                 } else {
                     mMessage.arg1 = AsynchronousGet.SUCCESS;
                     mMessage.obj = sougouSearchArticleList;
                 }
-            }else {
+            } else {
                 //服务器端无响应
                 mMessage.arg1 = AsynchronousGet.OBTAIN_RESULT_NULL;
             }
-            mHandler.sendMessage(mMessage);
         } catch (Exception e) {
-            Log.w("kinflow","解析搜狗响应体,出现错误:"+e.getMessage());
+            Log.w("kinflow", "解析搜狗响应体,出现错误:" + e.getMessage());
+            mMessage.arg1 = AsynchronousGet.RESPONSE_FAIL;
+        } finally {
+            return sougouSearchArticleList;
         }
 
     }
@@ -96,16 +100,16 @@ public class OkHttpPost {
     private List<SougouSearchArticle> parseSouGouSearchNews(JSONObject jsonObject) {
         List<SougouSearchArticle> sougouSearchArticleList = new ArrayList<>();
         try {
-            int status = jsonObject.optInt("status",0);//默认为出错
+            int status = jsonObject.optInt("status", 0);//默认为出错
             if (status == SougouSearchArticle.RESPONSE_STATUS_ERROR) {
-                    Log.w("Kinflow","搜狗搜索服务器有响应,但是状态错误,导致没有数据");
+                Log.w("Kinflow", "搜狗搜索服务器有响应,但是状态错误,导致没有数据");
             }
             if (status == SougouSearchArticle.RESPONSE_STATUS_SUCCESS) {//返回数据正常----正式开始解析数据
                 JSONArray sougouSearchNewsJsonArray = jsonObject.optJSONArray("result");
-                if (null==sougouSearchNewsJsonArray||
-                        sougouSearchNewsJsonArray.length()==0) {
-                    Log.w("Kinflow","获取到搜狗搜索新闻数据为空");
-                }else {
+                if (null == sougouSearchNewsJsonArray ||
+                        sougouSearchNewsJsonArray.length() == 0) {
+                    Log.w("Kinflow", "获取到搜狗搜索新闻数据为空");
+                } else {
                     for (int i = 0; i < sougouSearchNewsJsonArray.length(); i++) {
                         sougouSearchArticleList.add(new SougouSearchArticle(sougouSearchNewsJsonArray.optJSONObject(i)));
                     }
