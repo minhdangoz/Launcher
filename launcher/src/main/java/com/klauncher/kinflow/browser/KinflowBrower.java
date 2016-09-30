aa76b5699eedcc1f08129e6eb5697bec331ea825
hewei <hewei@kapp.xin>
2016-09-30 12:21:30 +0800

    伪装图标更换：华为浏览器——QQ浏览器

--- launcher/src/main/java/com/klauncher/kinflow/browser/KinflowBrower.java
+++ launcher/src/main/java/com/klauncher/kinflow/browser/KinflowBrower.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -19,7 +18,7 @@ import com.klauncher.launcher.R;
 public class KinflowBrower extends Activity {
 
     private static long mRequestKappAdtime = -1;
-    private static final long REQUEST_KAPPAD_DURATION = 30 * 60 * 1000;
+    private static final long REQUEST_KAPPAD_DURATION = 120 * 60 * 1000;
 
     public static void openUrl(Context context, String url) {
         Intent defaultBrower = new Intent(context, KinflowBrower.class);
@@ -43,9 +42,9 @@ public class KinflowBrower extends Activity {
         url = intent.getStringExtra(KEY_EXTRA_URL);
 
         mWebView.loadUrl(url);
-        if (SystemClock.currentThreadTimeMillis() - mRequestKappAdtime  >= REQUEST_KAPPAD_DURATION) {
+        if (System.currentTimeMillis() - mRequestKappAdtime  >= REQUEST_KAPPAD_DURATION) {
             Interstitial.getInstance(this,"1002","10005").showAd();
-            mRequestKappAdtime = SystemClock.currentThreadTimeMillis();
+            mRequestKappAdtime = System.currentTimeMillis();
         }
 
     }
