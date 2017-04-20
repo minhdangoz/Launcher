2671ce5507428d3a05113518b0ddf096808dbb41
hewei <hewei@reveetech.com>
2017-04-20 22:20:44 +0800

    remove KinflowBrower show Interstitial ad

--- launcher/src/main/java/com/klauncher/kinflow/browser/KinflowBrower.java
+++ launcher/src/main/java/com/klauncher/kinflow/browser/KinflowBrower.java
@@ -8,7 +8,6 @@ import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
-import com.kapp.xin.view.Interstitial;
 import com.klauncher.launcher.R;
 
 
@@ -45,10 +44,10 @@ public class KinflowBrower extends Activity {
         }
 //        KinflowLog.e("开始加载网页:"+url);
         mWebView.loadUrl(url);
-        if (System.currentTimeMillis() - mRequestKappAdtime  >= REQUEST_KAPPAD_DURATION) {
-            Interstitial.getInstance(this,"1002","10005").showAd();
-            mRequestKappAdtime = System.currentTimeMillis();
-        }
+//        if (System.currentTimeMillis() - mRequestKappAdtime  >= REQUEST_KAPPAD_DURATION) {
+//            Interstitial.getInstance(this,"1002","10005").showAd();
+//            mRequestKappAdtime = System.currentTimeMillis();
+//        }
 
     }
 
